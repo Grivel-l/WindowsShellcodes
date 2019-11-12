652de605819b76ad87678eea1fa9ba2a724f49a1
Grivel-l <legrivel@student.le-101.fr>
2019-11-12 03:20:48 +0100

    Adjust size for 64 bits

--- shellcode.s
+++ shellcode.s
@@ -3,9 +3,9 @@ global main
 section .text
   main:
     mov rax, [fs:0x30]    ; Get PEB
-    mov rax, [rax + 0x0c] ; PEB->PEB_DATA
-    mov rax, [rax + 0x14] ; ->InMemoryOrderModuleList
-    mov rax, [rax]        ; ntdll.dll
-    mov rax, [rax]        ; kernel32.dll
-    mov rax, [rax + 0x10] ; kernel32.dll base address
+    mov rax, [rax + 0x14] ; ->PPEB_LDR_DATA
+    mov rax, [rax + 0x20] ; ->LIST_ENTRY argv[0].exe
+    mov rax, [rax]        ; LIST_ENTRY ntdll.dll
+    mov rax, [rax]        ; LIST_ENTRY kernel32.dll
+    mov rax, [rax + 0x30] ; kernel32.dll base address
     jmp $
