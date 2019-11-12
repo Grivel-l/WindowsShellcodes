97f76e96ae1b4a663616a6a3216f6a15f5b14bcc
Grivel-l <legrivel@student.le-101.fr>
2019-11-12 17:43:06 +0100

    Get dll signature

--- shellcode.s
+++ shellcode.s
@@ -1,12 +1,15 @@
 global main
-extern _write
 
 section .text
   main:
     mov rax, [gs:0x60]    ; Get PEB
     mov rax, [rax + 0x18] ; ->PPEB_LDR_DATA
-    mov rax, [rax + 0x20] ; ->LIST_ENTRY argv[0].exe
+    mov rax, [rax + 0x10] ; ->LIST_ENTRY argv[0].exe
     mov rax, [rax]        ; LIST_ENTRY ntdll.dll
     mov rax, [rax]        ; LIST_ENTRY kernel32.dll
     mov rax, [rax + 0x30] ; kernel32.dll base address
-    jmp $
+    mov rbx, rax
+    add rbx, 0x3c
+    mov rcx, rax
+    add ecx, [ebx]        ; Signature here
+    int3
