248343690b1a5982b14e60d158b583dc767416d3
Grivel-l <legrivel@student.le-101.fr>
2019-11-14 17:50:29 +0100

    Clean code, create function to get function by name

--- shellcode.s
+++ shellcode.s
@@ -2,27 +2,28 @@ global main
 
 section .text
   main:
-    ; rax = IMAGE_DATA_DIRECTORY
-    ; rbx = k32 base adress
-    ; rcx = k32 header
     mov rcx, k32Name
-    call getLDTE
-    int3
-    mov rax, [rax]        ; LIST_ENTRY/LDR_DATA_TABLE_ENTRY ntdll.dll
-    mov rax, [rax]        ; LIST_ENTRY/LDR_DATA_TABLE_ENTRY kernel32.dll
-    mov rbx, [rax + 0x30] ; kernel32.dll base address
-    mov rcx, rbx
-    add rcx, 0x3c         ; rcx = lstnew
-    mov rax, rbx          ; rax = k32 base address
-    add eax, [ecx]        ; IMAGE_NT_HEADERS
-    add rax, 0x88         ; IMAGE_DATA_DIRECTORY - First one is export table
-    mov rsi, rbx
-    add esi, [eax]        ; rsi = IMAGE_EXPORT_DIRECTORY
-    mov esi, [esi + 0x20] ; rsi = Addres of RVA to RVA containing functions names
-    mov rdi, rbx
-    add edi, esi          ; RVA of array containing functions names
+    call getLDTE          ; rax = Kernel32 Image directory
+    mov rcx, rax
+    mov rdx, functionName
+    mov r8, functionNameLen
+    call getFunction
+  getFunction:
+    mov r10, [rax + 0x30] ; dll base address
+    mov r9, r10
+    add r9, 0x3c          ; r9 = RVA of PE signature
+    mov r11, r10
+    add r11d, [r9d]       ; IMAGE_NT_HEADERS
+    add r11, 0x88         ; IMAGE_DATA_DIRECTORY - First one is export table
+    mov r9, r10
+    add r9d, [r11d]
+    mov r9d, [r9d + 0x20] ; r9 = Addres of RVA to RVA containing functions names
+    mov r11, r10
+    add r11d, r9d         ; RVA of array containing functions names
     int3
   getLDTE:
+    push rdi
+    push rsi
     mov rax, [gs:0x60]    ; PEB
     mov rax, [rax + 0x18] ; PPEB_LDR_DATA
     mov rax, [rax + 0x10] ; LIST_ENTRY/LDR_DATA_TABLE_ENTRY argv[0]
@@ -42,8 +43,12 @@ section .text
           mov rax, [rax]
           jmp loop
     done:
+      pop rsi
+      pop rdi
       ret
 
 section .data
   k32Name db "K", 0, "E", 0, "R", 0, "N", 0, "E", 0, "L", 0, "3", 0, "2", 0, ".", 0, "d", 0, "l", 0, "l", 0
   k32Len equ ($ - k32Name) / 2
+  functionName db "AddAtomA", 0
+  functionNameLen equ $ - functionName
